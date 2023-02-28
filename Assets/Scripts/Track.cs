ec50d4637af30ad167aba98bf8da22dd27bd375d
Zachary Jones <>
2023-02-27 19:03:16 -0500

    added temp music controller for background music. added 8 tracks, UI for music control, and volume slider

---
+++ Assets/Scripts/Track.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Track")]
+public class Track : ScriptableObject
+{
+    public AudioClip trackAudioClip;
+}
