c2e3e91de24852e287a338f072be1b02258829e6
Philip Robibero <pasr.contact@gmail.com>
2023-04-06 13:02:07 -0400

    tool tip updates, created resource folder

--- Assets/Scripts/UI/ToolTip.cs
+++ Assets/Scripts/UI/ToolTip.cs
@@ -2,40 +2,43 @@ using UnityEngine.EventSystems;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using System.Text;
+using System.IO;
 using TMPro;
-
-[System.Serializable]
-public class PlayerInfo
-{
-
-}
 
 public class ToolTip : MonoBehaviour
 {
-    [SerializeField] TMPro.TMP_Text itemName;
-    [SerializeField] TMPro.TMP_Text itemDescription;
+    [SerializeField] TMPro.TMP_Text elementName;
+    [SerializeField] TMPro.TMP_Text description;
+    [SerializeField] TMPro.TMP_Text damage;
 
     private GameObject go;
     private int numOfChildren;
     private string goName;
 
-    [SerializeField] List<string> _goList = new List<string>();
+    public GameObject testObject;
+
+    [SerializeField] List<GameObject> _goList = new List<GameObject>();
+
+    public static ToolTip _instance;
+
+
+    //Singleton to make sure there is only one tool tip at a time
+    /*public void Awake()
+    {
+        if (_instance != null && _instance == this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }*/
 
     void Start()
     {
-
-        //create list from game object
-        //use _goList
-        CreateGameObjectList();
-
-
-        //COMPARE GAMEOBJECT LIST
-
-        //LOAD RELEVANT TEXT TO THE TOOL TIP
-
-        Debug.Log(_goList[2]);
+        CreateCSVList(testObject);
     }
 
     /// <summary>
@@ -50,17 +53,46 @@ public class ToolTip : MonoBehaviour
 
         for (int i = 0; i < numOfChildren; i++)
         {
-            _goList.Add(go.transform.GetChild(i).gameObject.name.ToLower());
+            _goList.Add(go.transform.GetChild(i).gameObject);
         }
     }
 
-    void CreateJSONList()
+    void CreateCSVList(GameObject goTest)
     {
+        //get game object tag
+        string goName =  goTest.name;
+        string goTag = goTest.tag;
+        
+        TextAsset data = Resources.Load<TextAsset>("Database - " + goTag);
+
+        //create rows
+        string[] textData = data.text.Split(new char[] { '\n' });
+
+        //create columns
+        for (int i = 1; i < textData.Length; i++)
+        {
+            //create rows
+            string[] row = textData[i].Split(new char[] { ',' });
+            Debug.Log("index = " + Array.IndexOf(row, row[i]) + "name = " + row[i]);
+
+        }
+
+        Debug.Log(textData[0]);
+
+        //find go name in array
+        for (int i = 0; i < textData.Length; i ++)
+        {
+            if (textData[i] == goName)
+            {
+                Debug.Log(textData[i]);
+            }
+            else
+            {
+                Debug.Log("Can't find.");
+            }
+        }
+        
 
     }
 
-    private bool IsMouseOverUI()
-    {
-        return EventSystem.current.IsPointerOverGameObject();
-    }
 }
