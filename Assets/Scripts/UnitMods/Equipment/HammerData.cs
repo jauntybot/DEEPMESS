7d058134a2b32c086ece115113cd523b40c2bae8
Jon T <jt.starewell@gmail.com>
2023-05-27 11:14:43 -0400

    hammer test

--- Assets/Scripts/UnitMods/Equipment/HammerData.cs
+++ Assets/Scripts/UnitMods/Equipment/HammerData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Equipment/BHammer")]
+[CreateAssetMenu(menuName = "Equipment/Hammer")]
 [System.Serializable]
 public class HammerData : EquipmentData
 {
