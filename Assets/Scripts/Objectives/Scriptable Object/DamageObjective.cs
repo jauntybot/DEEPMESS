5ceabe97f0e69a6293317bd808ff38feec044c18
Jon T <jauntybot.contact@gmail.com>
2024-01-24 13:02:37 -0500

    objective exclusivity and rebalancing

--- Assets/Scripts/Objectives/Scriptable Object/DamageObjective.cs
+++ Assets/Scripts/Objectives/Scriptable Object/DamageObjective.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Objective/Damage")]
 public class DamageObjective : Objective {
 
-    public enum ObjectiveType { SlagDamage, NailDamage, ShieldDamage, HammerDamage };
+    public enum ObjectiveType { SlagDamage, NailDamage, ShieldDamage, HammerDamage, EnemyOnEnemyDamage };
     [Header("Damage Conditions")]
     [SerializeField] ObjectiveType objectiveType;
 
@@ -29,6 +29,9 @@ public class DamageObjective : Objective {
             break;
             case ObjectiveType.HammerDamage:
                 if (evt.element is EnemyUnit && evt.sourceEquip is HammerData) progress++;
+            break;
+            case ObjectiveType.EnemyOnEnemyDamage:
+                if (evt.element is EnemyUnit && evt.source is EnemyUnit && evt.damageType != GridElement.DamageType.Crush) progress++;
             break;
         }
 
